e8526f301c5c53de4f91b89d1e4368a473849a3c
guydani <guy7145@gmail.com>
2017-06-21 22:25:00 +0300

    file loader 'load_collevtion2v3' + empty datacenter

--- code/fetureExtractorUtil.py
+++ code/fetureExtractorUtil.py
@@ -10,3 +10,15 @@ def derivation(list2):
     return result
 
 print(derivation([[1,2,3],[-5,5,6],[7,8,9]]))
+
+
+def derivation2(list2):
+    result = list()
+    for pair in [list2[i: i + 2] for i in range(len(list2)-1)]:
+        temp = list()
+        for x1,x2 in zip(pair[0], pair[1]):
+            temp.append(x2-x1)
+        result.append(temp)
+    return result
+
+print(derivation2([[1,2,3],[-5,5,6],[7,8,9]]))
--- definitions.py
+++ definitions.py
@@ -3,8 +3,8 @@ from log.Print import *
 print("initializing definitions...", COMMENT)
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-MORIARTY_DIR = ROOT_DIR + "/data/MoriartyDatasets/MoriartyDatasets1/"
-COLLECTION3V6_DIR = ROOT_DIR + "/data/collection3V2/"
+MORIARTY_DIR = ROOT_DIR + "/data/MoriartyDatasets/MoriartyDatasets1"
+COLLECTION3V6_DIR = ROOT_DIR + "/data/collection3V2"
 
 print("ROOT_DIR = " + ROOT_DIR, COMMENT)
 print("MORIARTY_DIR = " + MORIARTY_DIR, COMMENT)
