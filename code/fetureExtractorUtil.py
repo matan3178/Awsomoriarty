1968ac7fa192b573a1f48aa59a70db53adcdf7df
matankin <matan3178@gmail.com>
2017-06-21 22:28:29 +0300

    fixed a bug

--- code/fetureExtractorUtil.py
+++ code/fetureExtractorUtil.py
@@ -1,25 +1,39 @@
 from log.Print import *
+from definitions import NUMBER_OF_REDUNDENT_LINES
 import math
 
-def removeNullRows(list_of_lists):
-    warningFlag=0;
-    result=list();
+
+def start(data):
+    normalize=remove_null_rows(remove_first_line(data))
+    print(data_to_vector_size(normalize))
+    print(derivation(normalize))
+
+
+def remove_first_line(data):
+    return data[NUMBER_OF_REDUNDENT_LINES:]
+
+
+def remove_null_rows(list_of_lists):
+    warning_flag = 0;
+    result = list();
     for lst in list_of_lists:
         if 'null' in lst:
-            warningFlag+=1
-            if(warningFlag>1):
+            warning_flag+=1
+            if(warning_flag>1):
                 print("More than one null in a row!",WARNING)
         else:
             result.append(lst)
-            warningFlag=0
+            warning_flag=0
     return result
 
-def dataToVectorSize(list_of_lists):
-    result=list()
+
+def data_to_vector_size(list_of_lists):
+    result = list()
     for lst in list_of_lists:
         sum2 = math.sqrt(sum(x**2 for x in lst))
         result.append(sum2)
     return result
+
 
 def derivation(list2):
     result=list()
@@ -30,6 +44,8 @@ def derivation(list2):
         result.append(temp)
     return result
 
+
 print(derivation([[1,2,3],[-5,5,6],[7,8,9]]))
-print(dataToVectorSize([[1,2,3],[-5,5,6],[7,8,9]]))
-print(removeNullRows([[1,2,3],[4,'null',6],[7,6,5],[1,'null','null'],[4,5,'null'],[1,2,3],[4,'null',6],[7,6,5],[1,'null','null']]));
+print(data_to_vector_size([[1,2,3],[-5,5,6],[7,8,9]]))
+print(remove_null_rows([[1,2,3],[4,'null',6],[7,6,5],[1,'null','null'],[4,5,'null'],[1,2,3],[4,'null',6],[7,6,5],[1,'null','null']]));
+print(remove_first_line([[1,2,3],[-5,5,6],[7,8,9]]))
--- definitions.py
+++ definitions.py
@@ -5,6 +5,7 @@ print("initializing definitions...", COMMENT)
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 MORIARTY_DIR = ROOT_DIR + "/data/MoriartyDatasets/MoriartyDatasets1/"
 COLLECTION3V6_DIR = ROOT_DIR + "/data/collection3V2/"
+NUMBER_OF_REDUNDENT_LINES = 1
 
 print("ROOT_DIR = " + ROOT_DIR, COMMENT)
 print("MORIARTY_DIR = " + MORIARTY_DIR, COMMENT)
