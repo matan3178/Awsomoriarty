89a8298e5d7d931f5d22d6ead773cca5e850fe1d
guydani <guy7145@gmail.com>
2017-06-21 14:36:57 +0300

    hello world

---
+++ code/FileLoader.py
@@ -0,0 +1,26 @@
+import csv
+
+
+class FileLoader:
+
+    use_cache = True
+    cache = dict()
+
+    def __init__(self, use_cache):
+        self.use_cache = use_cache
+        return
+
+    def __init__(self):
+        return
+
+    def load_csv_file(self, path):
+        if self.use_cache and path in self.cache:
+            return self.cache[path]
+        else:
+            data = list()
+            with open(path) as datafile:
+                lines = enumerate(csv.reader(datafile, delimiter=","))
+                data.extend(lines)
+            if self.use_cache: self.cache[path] = data
+            return data
+        return
---
+++ code/utils.py
@@ -0,0 +1,7 @@
+def flatten_list(lst):
+    flat_list = list();
+
+    for el in lst:
+        flat_list.extend(el)
+
+    return flat_list
