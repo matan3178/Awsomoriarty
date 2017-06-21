88e9403c057bed80f01ca973859b1626a0d2dd19
guydani <guy7145@gmail.com>
2017-06-21 23:02:27 +0300

    load_csv got read of loaded line number

--- code/FetureExtractorUtil.py
+++ code/FetureExtractorUtil.py
@@ -1,28 +1,25 @@
-
 from log.Print import *
 from definitions import NUMBER_OF_REDUNDENT_LINES, NUMBER_OF_REDUNDENT_COLUMNS
 import math
 
 
 def start(data):
-    normalize=string_to_float(remove_null_rows(remove_redundent_lines_and_rows(data)))
-    (data_to_vector_size(normalize))
-    (derivation(normalize))
+    return data_to_vector_size(string_to_float(remove_null_rows(remove_redundent_lines_and_rows(data))))
 
 
 def string_to_float(data):
-    result=list()
+    result = list()
     for lst in data:
-        temp=list()
+        temp = list()
         for num in lst:
             temp.append(float(num))
         result.append(temp)
     return result
 
 
-
 def remove_redundent_lines_and_rows(data):
     return [sample[NUMBER_OF_REDUNDENT_COLUMNS:] for sample in data[NUMBER_OF_REDUNDENT_LINES:]]
+
 
 def remove_null_rows(list_of_lists):
     warning_flag = 0;
@@ -47,9 +44,9 @@ def data_to_vector_size(list_of_lists):
 
 
 def derivation(list2):
-    result=list()
+    result = list()
     for i in range(len(list2)-1):
-        temp=list()
+        temp = list()
         for j in range(len(list2[i])):
             temp.append(list2[i+1][j]-list2[i][j])
         result.append(temp)
@@ -60,4 +57,4 @@ print(derivation([[1,2,3],[-5,5,6],[7,8,9]]))
 print(data_to_vector_size([[1,2,3],[-5,5,6],[7,8,9]]))
 print(remove_null_rows([[1,2,3],[4,'null',6],[7,6,5],[1,'null','null'],[4,5,'null'],[1,2,3],[4,'null',6],[7,6,5],[1,'null','null']]));
 print(remove_redundent_lines_and_rows([[1,2,3],[-5,5,6],[7,8,9]]))
-print(stringToDouble([['1','2.5','3'],['-5','5','6'],['7','8','9']]))
+print(string_to_float([['1','2.5','3'],['-5','5','6'],['7','8','9']]))
