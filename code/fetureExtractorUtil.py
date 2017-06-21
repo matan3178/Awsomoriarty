929c7287cbce58a6581b3cbc632646647ddd7745
matankin <matan3178@gmail.com>
2017-06-21 21:48:40 +0300

    fixed a bug

--- code/fetureExtractorUtil.py
+++ code/fetureExtractorUtil.py
@@ -1,4 +1,25 @@
 from log.Print import *
+import math
+
+def removeNullRows(list_of_lists):
+    warningFlag=0;
+    result=list();
+    for lst in list_of_lists:
+        if 'null' in lst:
+            warningFlag+=1
+            if(warningFlag>1):
+                print("More than one null!",WARNING)
+        else:
+            result.append(lst)
+            warningFlag=0
+    return result
+
+def dataToVectorSize(list_of_lists):
+    result=list()
+    for lst in list_of_lists:
+        sum2 = math.sqrt(sum(x**2 for x in lst))
+        result.append(sum2)
+    return result
 
 def derivation(list2):
     result=list()
@@ -10,3 +31,5 @@ def derivation(list2):
     return result
 
 print(derivation([[1,2,3],[-5,5,6],[7,8,9]]))
+print(dataToVectorSize([[1,2,3],[-5,5,6],[7,8,9]]))
+print(removeNullRows([[1,2,3],[4,'null',6],[7,6,5],[1,'null','null'],[4,5,'null']]));
