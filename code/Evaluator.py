a2419ab4ea4ec5e6dd985eb4de86d07e4bb58b57
matankin <matan3178@gmail.com>
2017-07-03 20:02:13 +0300

    fixed a bug

---
+++ code/Evaluator.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class Evaluator:
+    def train(self, classifier, training_set, test_set_benign, test_set_fraud):
+        test_set = test_set_benign.extend(test_set_fraud)
+        test_labels = np.zeros(len(test_set_benign)).extend(np.ones(len(test_set_fraud)))
+        classifier.train(training_set)
+        prediction=classifier.predict(test_set)
+        self.evaluate(test_labels,prediction)
+
+    def evaluate(self, test_lables, prediction):
+        FP_counter=0
+        FN_counter=0
+        TP_counter=0
+        TN_counter=0
+        length = len(test_lables)
+        for t,p in test_lables,prediction:
+            if t==0 and p == 1:
+                FP_counter += 1
+            if t == 1 and p == 0:
+                FN_counter += 1
+            if t == 0 and p == 0:
+                TN_counter += 1
+            if t == 1 and p == 1:
+                TP_counter += 1
+        err_rate = (FP_counter + FN_counter) / length
+        recall = TP_counter / (TP_counter + FN_counter)
+        percision = TP_counter / (TP_counter + FP_counter)
+        specificity = TN_counter / (TN_counter + FP_counter)
+        false_alarm_rate = FP_counter  / (FP_counter + TN_counter)
+
+        print("generating svm...", HEADER)
